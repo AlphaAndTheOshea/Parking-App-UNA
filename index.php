470ac150a2e75910201f0550f78f05e6dc638bbd
Cody Daffinson <designbydaff@gmail.com>
2021-03-16 16:02:57 -0500

    1st commit

---
+++ index.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<!--
+To change this license header, choose License Headers in Project Properties.
+To change this template file, choose Tools | Templates
+and open the template in the editor.
+-->
+<html>
+    <head>
+        <meta charset="UTF-8">
+        <title></title>
+    </head>
+    <body>
+        <h1>UNA Parking Pal</h1>
+        <form action="docs/map.php" method="post">
+            <input type="submit" value="Click to enter">
+        </form>
+        <?php
+        // put your code here
+        ?>
+    </body>
+</html>
