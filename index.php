87cc738a81d85cd109084b8a6e1ad6acb59e102f
Alec Harris <aoharris98@gmail.com>
2021-04-12 20:14:58 -0500

    Change file extension

--- index.php
+++ index.php
@@ -47,6 +47,7 @@
       </div>
     </div>
   </div>
+  <?php ?>
 <!-- partial -->
   <script src='https://cdnjs.cloudflare.com/ajax/libs/jquery/3.4.0/jquery.min.js'></script><script  src="./script.js"></script>
 </body>
