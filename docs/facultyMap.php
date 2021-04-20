12c473b8929af7a84a64ab6b261ad6406f4263d5
unknown <thekaysev@gmail.com>
2021-04-20 16:48:06 -0500

    added reusable code and fixed residents map issue

--- docs/facultyMap.php
+++ docs/facultyMap.php
@@ -5,5 +5,30 @@
 </head>
 <body>
 	<img src="../img/FacultyParking.png">
+	<?php
+// define variables and set to empty values
+$filter = $motorcycleErr = "";
+
+if ($_SERVER["REQUEST_METHOD"] == "POST") {
+  if (empty($_POST["filter"])) {
+      null;
+  } else {
+    getPhoto();
+  }
+}
+
+function getPhoto() {
+    echo "<img src='../img/motorcycleMap.png'";
+    echo "</br>";
+}
+?>
+
+<form method="post" action="<?php echo htmlspecialchars($_SERVER["PHP_SELF"]);?>">  
+  Filters:
+  <input type="radio" name="filter" <?php if (isset($filter) && $filter=="motorcycle") echo "checked";?> value="motorcycle">Motorcycle
+  <span class="error"> <?php echo $motorcycleErr;?></span>
+  <br><br>
+  <input type="submit" name="submit" value="Submit">  
+</form>
 </body>
 </html>
