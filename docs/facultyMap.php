7c5ba2aaf3c6fee9a934103d726a94321a900e7c
thekaysev <48273558+thekaysev@users.noreply.github.com>
2021-04-20 16:59:27 -0500

    Update facultyMap.php

--- docs/facultyMap.php
+++ docs/facultyMap.php
@@ -18,7 +18,7 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
 }
 
 function getPhoto() {
-    echo "<img src='../img/motorcycleMap.png'";
+    echo "<img src='../img/MotorcycleMap.png'";
     echo "</br>";
 }
 ?>
