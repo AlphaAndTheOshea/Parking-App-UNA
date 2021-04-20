038b635abcdc5e5272d56174f2f3ca4d44522928
thekaysev <48273558+thekaysev@users.noreply.github.com>
2021-04-20 17:01:31 -0500

    Update residentMap.php

--- docs/residentMap.php
+++ docs/residentMap.php
@@ -1,19 +1,10 @@
 
-<?php
-
-echo "<h2>Testing for resident map page</h2>";
-
-$url = htmlspecialchars($_SERVER['HTTP_REFERER']);
-echo "<a href='$url'>back</a>";
-?>
-
 <!DOCTYPE html>
 <html>
 <head>
 	<title>Resident Parking</title>
 </head>
 <body>
-    <!insert the last part of the image address below**>
-	<img src="../img/">
+	<img src="../img/ResidentsParkingMap.png">
 </body>
 </html>
